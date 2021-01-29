6c43f26063374db3333385957a2f759067ca7f59
cnnrrss <14183191+cnnrrss@users.noreply.github.com>
2021-01-29 16:08:00 +1000

    feat: example scripts

---
+++ scripts/create_lifecycle_policy.sh
@@ -0,0 +1,24 @@
+# 1. Create a lifecylce policy (note this aPI is only supported by Elastic cloud and is not available on ESS)
+
+curl -X PUT "<es-domain>" -H 'Content-Type: application/json' -d'
+{
+  "policy": {
+    "phases": {
+      "hot": {
+        "actions": {
+          "rollover": {
+            "max_size": "50GB",
+            "max_age": "30d"
+          }
+        }
+      },
+      "delete": {
+        "min_age": "60d",
+        "actions": {
+          "delete": {}
+        }
+      }
+    }
+  }
+}
+'
